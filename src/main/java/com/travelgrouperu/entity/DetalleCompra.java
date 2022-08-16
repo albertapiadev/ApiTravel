7ccc2f00a4053935d833e087146c7cb0e18e44d0
Albert Tapia <albertapiadev@gmail.com>
2022-08-16 00:36:19 -0500

    Add detalle compra

--- src/main/java/com/travelgrouperu/entity/DetalleCompra.java
+++ src/main/java/com/travelgrouperu/entity/DetalleCompra.java
@@ -5,14 +5,19 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.persistence.*;
 import java.util.Date;
 
 @Setter
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity(name = "detalle_compra")
+@Table
 public class DetalleCompra {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int idDetalle;
     private String origen;
     private String destino;
